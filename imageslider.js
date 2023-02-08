e0c9fe59b9bd5331f0775ebf81f9c383f4692888
Sven1894 <svenloose@freenet.de>
2023-02-08 08:49:58 +0100

    min-height

---
+++ imageslider.js
@@ -0,0 +1,10 @@
+var slider = document.querySelector('.slider');
+
+$(document).ready(function () {
+    $(".slider").slick({
+      autoplay: true,
+      autoplaySpeed: 2500,
+      arrows: false,
+      dots: false
+    });
+  });
